90d5e603f0d8534af866f2fca4623ae6b172074d
gurmeet1011 <gurmeetrathorjnv1012@gmail.com>
2024-11-29 17:28:06 +0530

    error fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
-app.use(cors());
 
 import router from "./router/transaction-router.js";
 import connectDb from "./utils/db.js";
