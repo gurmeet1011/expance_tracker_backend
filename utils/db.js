4fe4edb9e7195c1987677b90e21eb5a8f1897836
gurmeet1011 <gurmeetrathorjnv1012@gmail.com>
2024-12-24 22:28:13 +0530

    remove newUrlParser

--- utils/db.js
+++ utils/db.js
@@ -5,16 +5,15 @@ const URI = process.env.MONGO_URI;
 const connectDb = async () => {
   try {
     console.log(URI);
-    
-    await mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+
+    // No need for `useNewUrlParser` and `useUnifiedTopology` in newer versions
+    await mongoose.connect(URI);
     console.log("Connection successful to DB");
   } catch (error) {
-    console.error(error,"Database connection failed");
+    console.error(error, "Database connection failed");
     process.exit(1); // Exiting with code 1 for failure
   }
 };
 
 export default connectDb;
+
